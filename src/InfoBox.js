393b5fec4269394bbe39326573b7d884625d8a60
Janmar Jose D. Roque Jr <janmarroquejr@gmail.com>
2020-09-05 18:43:28 +0800

    added core features

---
+++ src/InfoBox.js
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import "./InfoBox.css";
+
+function InfoBox({ title, cases, total }) {
+  return (
+    <div className="infoBox">
+      <Card>
+        {cases !== undefined && (
+          <CardContent>
+            <Typography className="infoBox__title" color="textSecondary">
+              {title}
+            </Typography>
+            <h2 className="infoBox__cases">{cases} today</h2>
+            <Typography className="infoBox__total" color="textSecondary">
+              {total} Total
+            </Typography>
+          </CardContent>
+        )}
+      </Card>
+    </div>
+  );
+}
+
+export default InfoBox;
